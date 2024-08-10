11b52409b91db8a716c161b0406e72921822a263
Ana Laura Marques <lauramarques181@gmail.com>
2024-08-10 20:58:01 -0300

    Wrapper intro

---
+++ src/Owrappers/test/WrapperTest.java
@@ -0,0 +1,12 @@
+package Owrappers.test;
+
+public class WrapperTest {
+    Byte byteW = 127;
+    Short shortW = 1;
+    Integer intW = 1;
+    Long longW = 10L;
+    Float floatW = 10F;
+    Double doubleW = 10D;
+    Character charW = 'W';
+    Boolean booleanW = true;
+}
