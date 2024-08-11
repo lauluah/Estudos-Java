b409c37544dd2b3ead5402b978ee6777a04f48b6
Ana Laura Marques <lauramarques181@gmail.com>
2024-08-10 21:12:19 -0300

    Wrapper intro

--- src/Owrappers/test/WrapperTest.java
+++ src/Owrappers/test/WrapperTest.java
@@ -1,12 +1,24 @@
 package Owrappers.test;
 
 public class WrapperTest {
-    Byte byteW = 127;
-    Short shortW = 1;
-    Integer intW = 1;
-    Long longW = 10L;
-    Float floatW = 10F;
-    Double doubleW = 10D;
-    Character charW = 'W';
-    Boolean booleanW = true;
+    public static void main(String[] args) {
+        Byte byteW = 127;
+        Short shortW = 1;
+        Integer intW = 1; // autoboxing
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = true;
+
+        int i = intW; // unboxing
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("true");
+
+
+        System.out.println(verdadeiro);
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('!'));
+        System.out.println(Character.toLowerCase('A'));
+    }
 }
