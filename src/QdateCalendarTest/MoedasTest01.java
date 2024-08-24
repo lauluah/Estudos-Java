50b9d5e29c4b86c219e256d129f6951aa940da15
Ana Laura Marques <lauramarques181@gmail.com>
2024-08-23 22:25:04 -0300

    docs: Add moeda com Locale

--- src/QdateCalendarTest/MoedasTest01.java
+++ src/QdateCalendarTest/MoedasTest01.java
@@ -1,21 +1,30 @@
 package QdateCalendarTest;
 
 import java.util.Currency;
+import java.util.Locale;
 
 public class MoedasTest01 {
     public static void main(String[] args) {
-        Currency currencyUSD = Currency.getInstance("USD");
-        Currency currencyEUR = Currency.getInstance("EUR");
-        Currency currencyBRL = Currency.getInstance("BRL");
+      import java.util.Currency;
+import java.util.Locale;
 
-        System.out.println("Código da moeda: " + currencyUSD.getCurrencyCode());
-        System.out.println("Símbolo da moeda: " + currencyUSD.getSymbol());
+        public class CurrencyExample {
+            public static void main(String[] args) {
+                Currency currencyUSD = Currency.getInstance(Locale.US);
+                Currency currencyEUR = Currency.getInstance(Locale.GERMANY);
+                Currency currencyBRL = Currency.getInstance(new Locale("pt", "BR"));
 
-        System.out.println("Código da moeda: " + currencyEUR.getCurrencyCode());
-        System.out.println("Símbolo da moeda: " + currencyEUR.getSymbol());
+                System.out.println("Código da moeda: " + currencyUSD.getCurrencyCode());
+                System.out.println("Símbolo da moeda: " + currencyUSD.getSymbol());
 
-        System.out.println("Código da moeda: " + currencyBRL.getCurrencyCode());
-        System.out.println("Símbolo da moeda: " + currencyBRL.getSymbol());
+                System.out.println("Código da moeda: " + currencyEUR.getCurrencyCode());
+                System.out.println("Símbolo da moeda: " + currencyEUR.getSymbol());
+
+                System.out.println("Código da moeda: " + currencyBRL.getCurrencyCode());
+                System.out.println("Símbolo da moeda: " + currencyBRL.getSymbol());
+            }
+        }
+
 
     }
 }
